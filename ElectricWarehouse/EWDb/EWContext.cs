bf55aa20a97049a456680c1dbebbc89e55bee95b
LeiYangGH <675686066@qq.com>
2017-06-10 11:08:13 +0800

    ef code first sample

---
+++ ElectricWarehouse/EWDb/EWContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWDb
+{
+    //https://msdn.microsoft.com/en-us/library/jj193542(v=vs.113).aspx
+    public class EWContext : DbContext
+    {
+        public EWContext(string connString)
+        {
+            this.Database.Connection.ConnectionString = connString;
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
