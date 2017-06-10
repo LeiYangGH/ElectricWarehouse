362dbf02cef77553908058269c867a2dbfde2800
LeiYangGH <675686066@qq.com>
2017-06-10 12:25:02 +0800

    Add-Migration Initial -IgnoreChanges

--- ElectricWarehouse/EWDb/Blog.cs
+++ ElectricWarehouse/EWDb/Blog.cs
@@ -10,7 +10,7 @@ namespace EWDb
     {
         public int BlogId { get; set; }
         public string Name { get; set; }
-
+        public string Url { get; set; }
         public virtual List<Post> Posts { get; set; }
     }
 }
